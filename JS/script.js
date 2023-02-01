05d6db581eeebb12d7af1531d69853322269ab3d
Davit Tkeshelashvili <dtkeshelashvili07@gmail.com>
2023-02-01 22:18:05 +0400

    new app creation

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,18 @@
 "use strict";
 
-console.log(5 === "5");
+const numberOfFilms = +prompt("How many movies have you watched?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies : {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const movieName = prompt("Name one of the last movies which you have watched", "");
+const movieRate = prompt("How would you rate it", "");
+
+personalMovieDB.movies[movieName] = movieRate;
+
+console.log(personalMovieDB);
